cee75e83405d844c379ef9ace9abac904ac1f56d
Alex Gutler <alex11jgt@hotmail.com>
2015-11-22 22:45:32 -0200

    oauth

--- config/app.php
+++ config/app.php
@@ -143,6 +143,10 @@ return [
 
         Bestmomo\Scafold\ScafoldServiceProvider::class,
 
+        // OAuth2
+        LucaDegasperi\OAuth2Server\Storage\FluentStorageServiceProvider::class,
+        LucaDegasperi\OAuth2Server\OAuth2ServerServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -202,6 +206,7 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Html'      => Illuminate\Html\HtmlFacade::class,
         'Form'      => Illuminate\Html\FormFacade::class,
+        'Authorizer' => LucaDegasperi\OAuth2Server\Facades\Authorizer::class,
     ],
 
 ];
